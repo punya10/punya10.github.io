f90ab7b4882f66894794d388ed68fd388e3788f1
Punya Jain <punya10@gmail.com>
2023-04-14 01:00:37 -0700

    Update uw-fix.user.js

--- rx/uw-fix.user.js
+++ rx/uw-fix.user.js
@@ -2,7 +2,7 @@
 // @name        uw-fix
 // @match    https://apps.uworld.com/courseapp/usmle/*
 // @grant       none
-// @version     0.4
+// @version     0.5
 // @author      Punya Jain
 // @description UWORLD-Anki Cards finder
 // @downloadURL https://punya10.github.io/rx/uw-fix.user.js
