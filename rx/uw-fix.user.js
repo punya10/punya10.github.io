8144c7ba93f41899ba6e237473546556645c9bec
Punya Jain <punya10@gmail.com>
2023-04-18 18:45:04 -0700

    Update uw-fix.user.js

--- rx/uw-fix.user.js
+++ rx/uw-fix.user.js
@@ -17,7 +17,7 @@ const ANKIURL = 'https://werk.asuscomm.com:8769';
 var ppts = [3000, 5000, 8000];
 
 var myStyle = `
-dialog { box-shadow: 0 2px 5px rgba(0,0,0,0.3); border: none; border-radius: 10px; opacity: 0.25; }
+dialog { box-shadow: 0 2px 5px rgba(0,0,0,0.3); border: none; border-radius: 10px; opacity: 0.69; }
 dialog::backdrop { background: linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.4)); animation: fade-in 0.5s; }
 @keyframes fade-in {
     from { opacity: 0; }
@@ -136,15 +136,79 @@ function onAttrChange(qid, el, filter, cb) {
     });
 }
 
+
+async function processQuestion() {
+    const q = {};
+    q.id = document.querySelector("span.question-id").textContent.replace(/[^\d]/g, '');
+    q.query = `tag:#AK_Step1_v12::#UWorld::${q.id}`;
+    q.nids = await akx('findNotes', { "query": q.query });
+    q.ninfo = await akx('notesInfo', { "notes": q.nids });
+    q.tags = [...new Set(q.ninfo.flatMap(note => note.tags))];
+    q.imgs = {};
+
+    async function getImgs(f) {
+        var i = [...new Set(q.ninfo.flatMap(note => extractAllText(note.fields[f].value)))].filter(e => e);
+        var p = i.map(img => akx('retrieveMediaFile', { "filename": img }));
+        return Promise.all(p).then(data => {
+            if (data.filter(d => d).length > 0) { q.imgs[f] = [] }
+            data.forEach((d, di) => { if (d) { q.imgs[f].push([i[di], `data:image/png;base64,${d}`]) } });
+        });
+    }
+
+    q.related = getTags(q.tags);
+    q.path = Object.fromEntries([...document.querySelectorAll('.standard-header')].map(s => [s.textContent, s.previousSibling.textContent]));
+    q.path.id = q.id;
+    await getImgs('Sketchy');
+    await getImgs('First Aid');
+    await getImgs('Pixorize');
+
+    async function addToQueue() {
+        var cards = q.ninfo.flatMap(n => n.cards);
+        var destination = ['UW', window.location.pathname.replace('/courseapp/usmle/v12/testinterface/launchtest/9300640/', '').split('/')[0]].join('::');
+        console.log(destination, cards);
+    }
+
+    await addToQueue();
+
+    function show() {
+        if (document.querySelector('#ankiInfo')) {
+            document.querySelector('#ankiInfo').remove();
+        }
+        var ankiInfo = document.createElement("div");
+        ankiInfo.id = 'ankiInfo';
+
+        q.imgs["First Aid"] && q.imgs["First Aid"].forEach(i => {
+            var img = document.createElement("img");
+            img.src = i[1];
+            img.style.maxWidth = "unset";
+            img.style.width = "100%";
+            ankiInfo.appendChild(img);
+        })
+        //document.querySelector(".standards").parentElement.insertBefore(ankiInfo, document.querySelector(".standards"));
+        document.querySelector('#first-explanation').appendChild(ankiInfo);
+    }
+
+    show();
+    console.log(await q);
+
+    //document.querySelector('#first-explanation > p:last-of-type').innerHTML = document.querySelector('#first-explanation > p:last-of-type').innerHTML.replace(/(\.(&nbsp;|\s)*|<br>[\n"]*)/g, '.<br>&#8226; ').replace(/\.<br>&#8226; $/g,'.');
+    return await q;
+}
+
+//var lnav = "#leftNavigator";
+var lnav = "#leftNavigator > .body";
+
+
 function initSync(auto = false, confirm = false) {
     akx().then(console.log).catch(console.error);
     var btn = document.createElement("button");
     btn.textContent = 'SYNC';
+    btn.style.width = "100%";
     btn.onclick = () => {
         let w = window.open('http://192.168.1.127:8766/sync', "_blank");
         setTimeout(() => w.close(), 1000);
     }
-    document.querySelector("#leftNavigator").appendChild(btn);
+    document.querySelector(lnav).appendChild(btn);
     if (auto) {
         btn.click();
     }
@@ -174,7 +238,7 @@ function ambossify(auto = false) {
                 document.head.appendChild(s);
             }
         }
-        document.querySelector("#leftNavigator").appendChild(btn);
+        document.querySelector(lnav).appendChild(btn);
         if (auto) {
             btn.click();
         }
@@ -185,7 +249,7 @@ function copyToClipboard(text) {
     navigator.clipboard.writeText(text);
 }
 
-function initHLs(last = 'highlighter_yellow') {
+function initHLs(last="highlighter-yellow") {
     var hb = {};
     hb.hls = [...document.querySelectorAll("i.fa-circle.fa-stack-1x.fas.highlighter")];
     hb.hls.forEach(h => {
@@ -198,26 +262,41 @@ function initHLs(last = 'highlighter_yellow') {
         };
 
 
-        var btn = document.createElement("button");
-        btn.id = hb[hn].name;
-        btn.textContent = hb[hn].short;
-        btn.style.backgroundColor = hb[hn].color;
-        btn.onclick = () => {
+       // hb[hn].btn = document.createElement("div");
+      
+        hb[hn].btn = document.createElement("button");
+        hb[hn].btn.id = hb[hn].name;
+        hb[hn].btn.textContent = hb[hn].short;
+        hb[hn].btn.style.backgroundColor = hb[hn].color;
+        hb[hn].btn.style.width = '33%';
+        hb[hn].btn.action = (e) => {
+          //if (hb[hn].btn.contains(e.target)) {
+            console.log("Clicked ME correctly",e);
             window.hlbtns.last = hb[hn];
+           console.log("clicked",hb[hn],window.hlbtns.last);
             h.click();
+          //}
+            
         }
-        hb[hn].btn = btn;
-        document.querySelector("#leftNavigator").appendChild(btn);
+        //hb[hn].btn.addEventListener('mouseup', btnFn);
+        hb[hn].btn.addEventListener('click', hb[hn].btn.action);
+        //hb[hn].btn = btn;
+        document.querySelector(lnav).appendChild(hb[hn].btn);
     });
     window.hlbtns = hb;
     window.hlbtns.last = window.hlbtns[last];
 
-    document.ontouchstart = (e) => window.getSelection().removeAllRanges();
-    document.ontouchend = (e) => {
-        copyToClipboard(window.getSelection().toString());
-        window.hlbtns.last.btn.click();
+  
+    document.querySelector("common-content").parentNode.ontouchstart = (e) => window.getSelection().removeAllRanges();
+    document.querySelector("common-content").parentNode.ontouchend = (e) => {
+      var curStr = window.getSelection().toString();
+      if(curStr.length > 0) {
+        
+        //copyToClipboard(window.getSelection().toString());
+        window.hlbtns.last.btn.action(e);
         //document.querySelectorAll("i.fa-circle.fa-stack-1x.fas.highlighter")[0].click();
         setTimeout(() => window.getSelection().removeAllRanges(), 50);
+      }
     }
 
 }
@@ -286,7 +365,7 @@ class Popup {
 class Popups {
     active = [];
     constructor(args) {
-        args.forEach(a => this.create(a));
+        //args.forEach(a => this.create(a));
     }
     create(arg) {
         this.active.push(new Popup(this.active,arg));
@@ -303,20 +382,26 @@ class Popups {
 function makePopups(qid) {
     window.qpps.cancelAll();
     if (document.querySelector('#explanation-container').hasAttribute("hidden")) { //explaination hidden, so make popups!
-        window.qpps.create({contents: "20", delay: 20000, timeout: 250, bg: '#01FF70'});
-        window.qpps.create({contents: "40", delay: 40000, timeout: 250, bg: '#FFDC00'});
-        window.qpps.create({contents: "60...MOVE ON!", delay: 60000, timeout: 250, bg: '#F012BE'});
+        window.qpps.create({contents: "20", delay:  2000, timeout: 691, bg: '#01FF70'});
+        window.qpps.create({contents: "40", delay: 4000, timeout: 691, bg: '#FFDC00'});
+        window.qpps.create({contents: "60...MOVE ON!", delay: 6000, timeout: 691, bg: '#F012BE'});
     }
 }
+
 
 var lastqid = 0;
 waitEl('common-content').then(cc => {
     var myCss = addcss(myStyle);
+  	
+  
     window.qpps = new Popups();
+    
     initSync(false, false);
     initHLs();
     ambossify(false);
 
+  
+  
 
     waitEl('.question-id').then(qel => {
         onCharChange(qel, async (qid) => {
@@ -343,62 +428,3 @@ waitEl('common-content').then(cc => {
         })
     });
 });
-
-
-async function processQuestion() {
-    const q = {};
-    q.id = document.querySelector("span.question-id").textContent.replace(/[^\d]/g, '');
-    q.query = `tag:#AK_Step1_v12::#UWorld::${q.id}`;
-    q.nids = await akx('findNotes', { "query": q.query });
-    q.ninfo = await akx('notesInfo', { "notes": q.nids });
-    q.tags = [...new Set(q.ninfo.flatMap(note => note.tags))];
-    q.imgs = {};
-
-    async function getImgs(f) {
-        var i = [...new Set(q.ninfo.flatMap(note => extractAllText(note.fields[f].value)))].filter(e => e);
-        var p = i.map(img => akx('retrieveMediaFile', { "filename": img }));
-        return Promise.all(p).then(data => {
-            if (data.filter(d => d).length > 0) { q.imgs[f] = [] }
-            data.forEach((d, di) => { if (d) { q.imgs[f].push([i[di], `data:image/png;base64,${d}`]) } });
-        });
-    }
-
-    q.related = getTags(q.tags);
-    q.path = Object.fromEntries([...document.querySelectorAll('.standard-header')].map(s => [s.textContent, s.previousSibling.textContent]));
-    q.path.id = q.id;
-    await getImgs('Sketchy');
-    await getImgs('First Aid');
-    await getImgs('Pixorize');
-
-    async function addToQueue() {
-        var cards = q.ninfo.flatMap(n => n.cards);
-        var destination = ['UW', window.location.pathname.replace('/courseapp/usmle/v12/testinterface/launchtest/9300640/', '').split('/')[0]].join('::');
-        console.log(destination, cards);
-    }
-
-    await addToQueue();
-
-    function show() {
-        if (document.querySelector('#ankiInfo')) {
-            document.querySelector('#ankiInfo').remove();
-        }
-        var ankiInfo = document.createElement("div");
-        ankiInfo.id = 'ankiInfo';
-
-        q.imgs["First Aid"] && q.imgs["First Aid"].forEach(i => {
-            var img = document.createElement("img");
-            img.src = i[1];
-            img.style.maxWidth = "unset";
-            img.style.width = "100%";
-            ankiInfo.appendChild(img);
-        })
-        //document.querySelector(".standards").parentElement.insertBefore(ankiInfo, document.querySelector(".standards"));
-        document.querySelector('#first-explanation').appendChild(ankiInfo);
-    }
-
-    show();
-    console.log(await q);
-
-    //document.querySelector('#first-explanation > p:last-of-type').innerHTML = document.querySelector('#first-explanation > p:last-of-type').innerHTML.replace(/(\.(&nbsp;|\s)*|<br>[\n"]*)/g, '.<br>&#8226; ').replace(/\.<br>&#8226; $/g,'.');
-    return await q;
-}
