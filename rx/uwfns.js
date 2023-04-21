ef911e6f907a3d707585d29ebd0adc9c77e75909
Punya Jain <punya10@gmail.com>
2023-04-21 08:09:40 -0700

    made uw class

--- rx/uwfns.js
+++ rx/uwfns.js
@@ -1,8 +1,14 @@
-const getAns = (a)=>`${a}: '${[...document.querySelectorAll('[id^="answerhighlight"]')][`${a}`.charCodeAt(0) - 65].textContent}'`;
-[...document.querySelectorAll('strong')].filter(c=>c.textContent.startsWith('(Choice')).forEach(c=>{
-    console.log(c.textContent);
-    var str = c.textContent;
-    str.match(/ ([A-Z])/g).forEach(a => {str = str.replace(a, "" + a + ": '" + [...document.querySelectorAll('[id^="answerhighlight"]')][a.charCodeAt(1)-65].textContent + "'")})
-    c.textContent = str;    
+class UW {
+    alert = true;
+    constructor() {
+      this.test = "hello";
+      this.log(this.test);
+    }
+    log(...args) {
+        console.log(...args);
+        if (this.alert) window.alert(args);
+    }
+    clearSelection() {
+        window.getSelection ? window.getSelection().empty ? window.getSelection().empty() : window.getSelection().removeAllRanges && window.getSelection().removeAllRanges() : document.selection && document.selection.empty()
+    }
 }
-);
