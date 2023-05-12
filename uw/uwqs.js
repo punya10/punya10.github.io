ca2e20835e22e0eb8868d6f11057b7c5114a4a83
Punya Jain <punya10@gmail.com>
2023-05-12 11:28:35 +0000

    basics

--- uw/uwqs.js
+++ uw/uwqs.js
@@ -1,6 +1,9 @@
+//https://insiders.vscode.dev/tunnel/devlocal/Users/punya
 const hy = require('./hy_rand.json');
 const ly = require('./ly_rand.json');
 const qy = require('./qs_rand.json');
 const nancy = require('./nancy.json');
 
 
+
+console.log(nancy);
