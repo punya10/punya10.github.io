e752dc9438b640f47c9673c2fefba0b24b8d7bdf
Punya Jain <punya10@gmail.com>
2023-05-12 04:35:00 +0000

    May 12, 2023, 4:35 AM

--- uw/uwqs.js
+++ uw/uwqs.js
@@ -3,3 +3,6 @@ const ly = require('./ly_rand.json');
 const qy = require('./qs_rand.json');
 const nancy = require('./nancy.json');
 
+
+console.log('hy', hy.isCorrect.length + hy.isIncorrect.length + hy.isOmitted.length)
+
